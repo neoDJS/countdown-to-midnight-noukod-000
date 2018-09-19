4e4a8795f69d11114ffbcadbb00de3d6c34fab6b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-19 14:02:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(sec)
+  while sec != 0
+    puts "#{sec} SECOND(S)!"
+    sec -= 1
+  end
+  test = "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(sec)
+  while sec != 0
+    puts "#{sec} SECOND(S)!"
+    sec -= 1
+    sleep(1)
+  end
+  test = "HAPPY NEW YEAR!"
 end
